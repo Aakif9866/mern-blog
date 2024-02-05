bf0b15b968408d1e3bf9323a7342398ae59be0e6
Aakif9866 <aakif9866@gmail.com>
2024-02-05 19:29:42 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+// we will define all the data to be taken defined for each  user
+// so we want this data to follow some specific rules
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
