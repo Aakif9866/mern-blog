6cf7330dfe91e790670fa363ce6448d38528166c
Aakif9866 <aakif9866@gmail.com>
2024-02-05 12:54:22 +0530

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
+// backend
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// connect db to backend
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("mongo db is connected");
+});
 
 const app = express();
 
