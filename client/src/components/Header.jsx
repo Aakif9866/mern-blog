bd27e745e023d5e550fb9fddbc329a2027ea5b12
Aakif9866 <aakif9866@gmail.com>
2024-03-02 23:28:05 +0530

    user data added to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { Navbar } from "flowbite-react";
+import { Navbar, Dropdown, Avatar } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { TextInput, Button } from "flowbite-react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
 
 function Header() {
-  const path = useLocation().pathname;
+  const path = useLocation().pathname; // to get the current location from the url
+  //  to update the location with the help of active in Nav.links
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user); // we need this inorder to display some of the attributes in
+  // The navbar
+
+  const handleSignout = () => {};
 
   return (
     <Navbar className="border-b-2">
@@ -41,14 +48,37 @@ function Header() {
         >
           <FaMoon />
         </Button>
-        <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" outline>
-            Sign in
-          </Button>
-        </Link>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
         <Navbar.Toggle />
       </div>
-
+      {/* the below component by default works in that way  */}
+      {/* on right screen it collapses and the icon comes at right most  */}
       <Navbar.Collapse>
         <Navbar.Link active={path === "/"} as={"div"}>
           <Link to="/">Home</Link>
