dd513d3e7a86d2542ba57139b04c5d89d00289b5
Aakif9866 <aakif9866@gmail.com>
2024-03-05 19:17:39 +0530

    Complete sidebar of the dashboard added

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { Sidebar } from "flowbite-react";
+import {
+  HiUser,
+  HiArrowSmRight,
+  HiDocumentText,
+  HiOutlineUserGroup,
+  HiAnnotation,
+  HiChartPie,
+} from "react-icons/hi";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+
+export default function DashSidebar() {
+  const location = useLocation();
+
+  const [Tab, setTab] = useState(""); // to get tab location
+
+  useEffect(() => {
+    const urlparams = new URLSearchParams(location.search); // put the url here
+    const tabfromUrl = urlparams.get("tab"); // tell the exact value of tab ==> to goto that page
+    if (tabfromUrl) {
+      setTab(tabfromUrl);
+    }
+  }, [location.search]); // whenever this changes useEffect start the work
+  return (
+    <Sidebar className="w-full">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={Tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelcolor="dark"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item classname="cursor-pointer" icon={HiArrowSmRight}>
+            Sign Out{" "}
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
+import { useLocation } from "react-router-dom"; // to find the tab location
+import { useState } from "react";
+
+// useState to set tab , uselocation to get the location
+// useEffect to search for the tab and to set it
+
+// dashsidebar on left and profile on right side -> 2 different components
+// also this page is locked only for the users who are signed in
+// URLSearchParams is a built-in JavaScript object that provides methods for working with the query string of a URL.
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+
+  const [Tab, setTab] = useState(""); // to get tab location
+
+  useEffect(() => {
+    const urlparams = new URLSearchParams(location.search); // put the url here
+    const tabfromUrl = urlparams.get("tab"); // tell the exact value of tab ==> to goto that page
+    if (tabfromUrl) {
+      setTab(tabfromUrl);
+    }
+  }, [location.search]); // whenever this changes useEffect start the work
+
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      {/* profile => will be shown only on profile tab */}
+      {Tab === "profile" && <DashProfile />}
+    </div>
+  );
 }
 
 export default Dashboard;
