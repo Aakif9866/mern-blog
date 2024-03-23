2e147cbbffe06c03bb95ceafdd5da1b1e7cd5e22
Aakif9866 <aakif9866@gmail.com>
2024-03-23 14:50:02 +0530

    create getusers api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { deleteUser, signout, test } from "../controller/user.controller.js"; // .js very very important
+import {
+  deleteUser,
+  signout,
+  test,
+  getUser,
+  getUsers,
+} from "../controller/user.controller.js"; // .js very very important
 import { updateUser } from "../controller/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -9,6 +15,8 @@ router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser); // update data
 router.delete("/delete/:userId", verifyToken, deleteUser); // delete user
 router.post("/signout", signout); // anybody can signout So no need of verifyToken
+router.get("/getusers", verifyToken, getUsers); // only can be done by admin
+router.get("/:userId", getUser);
 export default router;
 
 // Express is a web application framework for Node.js, and Express Router is a part of it. Express Router is used to create modular, mountable route handlers. It allows you to define routes in separate files and then use them in your main application.
