be43d592836f3369bcb11cb9be21dd64afa50cc8
Aakif9866 <aakif9866@gmail.com>
2024-04-08 21:17:19 +0530

    Add edit functionality to edit component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -193,6 +193,33 @@ export default function CommentSection({ postId }) {
       )}
 
       {/* finally the modal to delete comment */}
+      <Modal
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        popup
+        size="md"
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
+            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
+              Are you sure you want to delete this comment?
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button
+                color="failure"
+                onClick={() => handleDelete(commentToDelete)}
+              >
+                Yes, I'm sure
+              </Button>
+              <Button color="gray" onClick={() => setShowModal(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
