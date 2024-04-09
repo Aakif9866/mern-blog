569e662ded5a5431b6d0a293549b1f2d1bc2506c
Aakif9866 <aakif9866@gmail.com>
2024-04-10 02:37:47 +0530

    gina

--- api/index.js
+++ api/index.js
@@ -44,7 +44,7 @@ app.use("/api/comment", commentRoutes); // comment path
 // these are solely for deployment
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.use("*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
